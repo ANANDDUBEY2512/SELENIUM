e9162e62338c22b25dad9727ea693a996bb7abad
ANANDDUBEY2512 <ananddubey2512@gmail.com>
2021-04-03 10:36:09 +0530

    Update TestBase.java

--- POM/PageObjectModel-master/src/main/java/com/crm/qa/base/TestBase.java
+++ POM/PageObjectModel-master/src/main/java/com/crm/qa/base/TestBase.java
@@ -2,9 +2,8 @@ package com.crm.qa.base;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
